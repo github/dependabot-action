e12b5f1f4920b057078846b588b1a2face89b78d
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-06-04 11:39:41 -0400

    dockerode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {getInputs} from './inputs'
+import Docker from 'dockerode'
+import {runFileFetcher} from './updater/fetcher'
 
 async function run(): Promise<void> {
   try {
@@ -8,8 +10,13 @@ async function run(): Promise<void> {
     if (input === null) {
       return
     }
-
     core.info(`processing job: ${JSON.stringify(input)}`)
+
+    // TODO: api client: fetch job details
+
+    // TODO: the full docker jamboree
+    const docker = new Docker()
+    await runFileFetcher(docker)
   } catch (error) {
     core.setFailed(error.message)
   }
