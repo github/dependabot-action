ec342a72a3821f7e2d49a17ca8b5667fb06c6ed7
Barry Gordon <brrygrdn@github.com>
2021-08-24 11:12:08 +0100

    Remove support for repository dispatch

--- src/inputs.ts
+++ src/inputs.ts
@@ -1,24 +1,17 @@
 import * as core from '@actions/core'
 import {Context} from '@actions/github/lib/context'
-import {
-  RepositoryDispatchEvent,
-  WorkflowDispatchEvent
-} from '@octokit/webhooks-definitions/schema'
+import {WorkflowDispatchEvent} from '@octokit/webhooks-definitions/schema'
 import {JobParameters} from './api-client'
-
-// FIXME: '@octokit/webhooks-definitions' assumes this is the only repository_dispatch event type, workaround that
-// https://github.com/octokit/webhooks/blob/0b04a009507aa35811e91a10703bbb2a33bdeff4/payload-schemas/schemas/repository_dispatch/on-demand-test.schema.json#L14
-export const DISPATCH_EVENT_NAME = 'on-demand-test'
 
 export function getJobParameters(ctx: Context): JobParameters | null {
   switch (ctx.eventName) {
     case 'dynamic':
     case 'workflow_dispatch':
       return fromWorkflowInputs(ctx)
-    case 'repository_dispatch':
-      return fromRepoDispatch(ctx)
   }
-  core.debug(`Unknown event name: ${ctx.eventName}`)
+  core.info(
+    `Dependabot Updater Action does not support '${ctx.eventName}' events.`
+  )
   return null
 }
 
@@ -29,20 +22,5 @@ function fromWorkflowInputs(ctx: Context): JobParameters {
     evt.inputs.jobToken as string,
     evt.inputs.credentialsToken as string,
     evt.inputs.dependabotAPIURL as string
-  )
-}
-
-function fromRepoDispatch(ctx: Context): JobParameters | null {
-  const evt = ctx.payload as RepositoryDispatchEvent
-  if (evt.action !== DISPATCH_EVENT_NAME) {
-    core.debug(`skipping repository_dispatch for ${evt.action}`)
-    return null
-  }
-  const payload = evt.client_payload
-  return new JobParameters(
-    payload.jobID as number,
-    payload.jobToken as string,
-    payload.credentialsToken as string,
-    payload.dependabotAPIURL as string
   )
 }
