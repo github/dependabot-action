0ed5d3f4532f3d60fe606f3436163b5c6789c672
Barry Gordon <brrygrdn@github.com>
2021-09-20 21:01:00 +0100

    Updater now raises on container failures or empty output
    Co-authored-by: Barry Gordon <896971+brrygrdn@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -70843,7 +70843,7 @@ module.exports = eval("require")("encoding");
 /***/ ((module) => {
 
 "use strict";
-module.exports = JSON.parse('{"_args":[["axios@0.21.4","/Users/brrygrdn/Code/github/updater-action"]],"_from":"axios@0.21.4","_id":"axios@0.21.4","_inBundle":false,"_integrity":"sha512-ut5vewkiu8jjGBdqpM44XxjuCjq9LAKeHVmoVfHVzy8eHgxxq8SbAVQNovDA8mVi05kP0Ea/n/UzcSHcTJQfNg==","_location":"/axios","_phantomChildren":{},"_requested":{"type":"version","registry":true,"raw":"axios@0.21.4","name":"axios","escapedName":"axios","rawSpec":"0.21.4","saveSpec":null,"fetchSpec":"0.21.4"},"_requiredBy":["/"],"_resolved":"https://registry.npmjs.org/axios/-/axios-0.21.4.tgz","_spec":"0.21.4","_where":"/Users/brrygrdn/Code/github/updater-action","author":{"name":"Matt Zabriskie"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"bugs":{"url":"https://github.com/axios/axios/issues"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}],"dependencies":{"follow-redirects":"^1.14.0"},"description":"Promise based HTTP client for the browser and node.js","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"homepage":"https://axios-http.com","jsdelivr":"dist/axios.min.js","keywords":["xhr","http","ajax","promise","node"],"license":"MIT","main":"index.js","name":"axios","repository":{"type":"git","url":"git+https://github.com/axios/axios.git"},"scripts":{"build":"NODE_ENV=production grunt build","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","examples":"node ./examples/server.js","fix":"eslint --fix lib/**/*.js","postversion":"git push && git push --tags","preversion":"npm test","start":"node ./sandbox/server.js","test":"grunt test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json"},"typings":"./index.d.ts","unpkg":"dist/axios.min.js","version":"0.21.4"}');
+module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"Promise based HTTP client for the browser and node.js","main":"index.js","scripts":{"test":"grunt test","start":"node ./sandbox/server.js","build":"NODE_ENV=production grunt build","preversion":"npm test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json","postversion":"git push && git push --tags","examples":"node ./examples/server.js","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","fix":"eslint --fix lib/**/*.js"},"repository":{"type":"git","url":"https://github.com/axios/axios.git"},"keywords":["xhr","http","ajax","promise","node"],"author":"Matt Zabriskie","license":"MIT","bugs":{"url":"https://github.com/axios/axios/issues"},"homepage":"https://axios-http.com","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"jsdelivr":"dist/axios.min.js","unpkg":"dist/axios.min.js","typings":"./index.d.ts","dependencies":{"follow-redirects":"^1.14.0"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}],"_resolved":"https://registry.npmjs.org/axios/-/axios-0.21.4.tgz","_integrity":"sha512-ut5vewkiu8jjGBdqpM44XxjuCjq9LAKeHVmoVfHVzy8eHgxxq8SbAVQNovDA8mVi05kP0Ea/n/UzcSHcTJQfNg==","_from":"axios@0.21.4"}');
 
 /***/ }),
 
@@ -71144,7 +71144,7 @@ class ApiClient {
     constructor(client, params) {
         this.client = client;
         this.params = params;
-        // We use a static unknown SHA when making a job as complete from the action
+        // We use a static unknown SHA when marking a job as complete from the action
         // to remain in parity with the existing runner.
         this.UnknownSha = {
             'base-commit-sha': 'unknown'
@@ -71544,12 +71544,13 @@ const REPO_CONTENTS_PATH = '/home/dependabot/dependabot-updater/repo';
 const updater_CA_CERT_INPUT_PATH = '/usr/local/share/ca-certificates';
 const CA_CERT_FILENAME = 'dbot-ca.crt';
 class Updater {
-    constructor(updaterImage, proxyImage, apiClient, details, credentials) {
+    constructor(updaterImage, proxyImage, apiClient, details, credentials, outputPath = '../output/output.json') {
         this.updaterImage = updaterImage;
         this.proxyImage = proxyImage;
         this.apiClient = apiClient;
         this.details = details;
         this.credentials = credentials;
+        this.outputPath = outputPath;
         this.docker = new (docker_default())();
     }
     /**
@@ -71557,29 +71558,15 @@ class Updater {
      */
     runUpdater() {
         return updater_awaiter(this, void 0, void 0, function* () {
+            const proxy = yield new ProxyBuilder(this.docker, this.proxyImage).run(this.details, this.credentials);
+            proxy.container.start();
             try {
-                const proxy = yield new ProxyBuilder(this.docker, this.proxyImage).run(this.details, this.credentials);
-                proxy.container.start();
-                try {
-                    const files = yield this.runFileFetcher(proxy);
-                    if (!files) {
-                        core.error(`failed during fetch, skipping updater`);
-                        // TODO: report job runner_error?
-                        return;
-                    }
-                    yield this.runFileUpdater(proxy, files);
-                }
-                catch (e) {
-                    // TODO: report job runner_error?
-                    core.error(`Error ${e}`);
-                }
-                finally {
-                    yield this.cleanup(proxy);
-                }
+                const files = yield this.runFileFetcher(proxy);
+                yield this.runFileUpdater(proxy, files);
+                return true;
             }
-            catch (e) {
-                // TODO: report job runner_error?
-                core.error(`Error ${e}`);
+            finally {
+                yield this.cleanup(proxy);
             }
         });
     }
@@ -71589,9 +71576,9 @@ class Updater {
             yield ContainerService.storeInput(JOB_INPUT_FILENAME, JOB_INPUT_PATH, container, { job: this.details });
             yield ContainerService.storeCert(CA_CERT_FILENAME, updater_CA_CERT_INPUT_PATH, container, proxy.cert);
             yield ContainerService.run(container);
-            const outputPath = external_path_default().join(__dirname, '../output/output.json');
+            const outputPath = external_path_default().join(__dirname, this.outputPath);
             if (!external_fs_default().existsSync(outputPath)) {
-                return;
+                throw new Error('No output.json created by the fetcher container');
             }
             const fileFetcherSync = external_fs_default().readFileSync(outputPath).toString();
             const fileFetcherOutput = JSON.parse(fileFetcherSync);
