81e48c33dceef1e71490f76f8afc683aae33e02d
Philip Harrison <philip@mailharrison.com>
2022-02-01 14:54:59 -0600

    Extract updater/proxy tags from Dockerfile
    This could allow Dependabot to keep the `Dockerfile` up-to-date, and we
    wouldn't need another build step to use them.
    
    When running `ncc build` the referenced `Dockerfile` gets copied into
    `dist/main` so it's available to the code running in actions.
    
    Following on from: https://github.com/dependabot/updater-action/pull/72
    Working towards: github/dependabot-updates#2102

--- src/docker-tags.ts
+++ src/docker-tags.ts
@@ -1,4 +1,26 @@
-export const UPDATER_IMAGE_NAME =
-  'docker.pkg.github.com/dependabot/dependabot-updater:v1'
-export const PROXY_IMAGE_NAME =
-  'docker.pkg.github.com/github/dependabot-update-job-proxy:v1'
+import fs from 'fs'
+
+const dockerfile = fs.readFileSync(require.resolve('../Dockerfile'), 'utf8')
+
+const imageNames = dockerfile
+  .split(/\n/)
+  .filter(a => a.startsWith('FROM'))
+  .map(a => a.replace('FROM', '').trim())
+
+const updaterImageName = imageNames.find(a =>
+  a.includes('dependabot/dependabot-updater')
+)
+const proxyImageName = imageNames.find(a =>
+  a.includes('github/dependabot-update-job-proxy')
+)
+
+if (!updaterImageName) {
+  throw new Error('Could not find dependabot-updater image name')
+}
+
+if (!proxyImageName) {
+  throw new Error('Could not find dependabot-update-job-proxy image name')
+}
+
+export const UPDATER_IMAGE_NAME = updaterImageName
+export const PROXY_IMAGE_NAME = proxyImageName
