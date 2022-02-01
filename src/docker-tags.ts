b52afece4cd4820201a79f800726debefb81d58c
Philip Harrison <philip@mailharrison.com>
2022-02-01 14:41:27 -0600

    Revert to working v1 tags

--- src/docker-tags.ts
+++ src/docker-tags.ts
@@ -1,4 +1,4 @@
 export const UPDATER_IMAGE_NAME =
-  'docker.pkg.github.com/dependabot/dependabot-updater@sha256:58e0fb2e0373294b05ad5e8af7d190d47a1bb3e098426c3840a31db0b0b0a931'
+  'docker.pkg.github.com/dependabot/dependabot-updater:v1'
 export const PROXY_IMAGE_NAME =
-  'docker.pkg.github.com/github/dependabot-update-job-proxy@sha256:835ce18d93a9ba7336ed815cec7d43b78649cd1ce63b4559fb00f0726658a8df'
+  'docker.pkg.github.com/github/dependabot-update-job-proxy:v1'
