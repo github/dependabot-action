1a6183f51da26f21bca2100f44beb3429af69af2
Barry Gordon <brrygrdn@github.com>
2022-02-22 17:14:22 +0000

    Ingest the docker/containers.json config

--- src/docker-tags.ts
+++ src/docker-tags.ts
@@ -1,26 +1,4 @@
-import fs from 'fs'
+import dockerContainerConfig from '../docker/containers.json'
 
-const dockerfile = fs.readFileSync(require.resolve('../Dockerfile'), 'utf8')
-
-const imageNames = dockerfile
-  .split(/\n/)
-  .filter(a => a.startsWith('FROM'))
-  .map(a => a.replace('FROM', '').trim())
-
-const updaterImageName = imageNames.find(a =>
-  a.includes('dependabot/dependabot-updater')
-)
-const proxyImageName = imageNames.find(a =>
-  a.includes('github/dependabot-update-job-proxy')
-)
-
-if (!updaterImageName) {
-  throw new Error('Could not find dependabot-updater image name')
-}
-
-if (!proxyImageName) {
-  throw new Error('Could not find dependabot-update-job-proxy image name')
-}
-
-export const UPDATER_IMAGE_NAME = updaterImageName
-export const PROXY_IMAGE_NAME = proxyImageName
+export const UPDATER_IMAGE_NAME = dockerContainerConfig.updater
+export const PROXY_IMAGE_NAME = dockerContainerConfig.proxy
