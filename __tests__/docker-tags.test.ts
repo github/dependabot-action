2d7ada18eac80851c7222e530cd3e3c88a414d19
Barry Gordon <brrygrdn@github.com>
2022-02-23 18:14:59 +0000

    Switch to using explicit container SHAs

--- __tests__/docker-tags.test.ts
+++ __tests__/docker-tags.test.ts
@@ -3,13 +3,13 @@ import {UPDATER_IMAGE_NAME, PROXY_IMAGE_NAME} from '../src/docker-tags'
 describe('Docker tags', () => {
   test('UPDATER_IMAGE_NAME', () => {
     expect(UPDATER_IMAGE_NAME).toMatch(
-      /^docker\.pkg\.github\.com\/dependabot\/dependabot-updater:v1$/
+      /^docker\.pkg\.github\.com\/dependabot\/dependabot-updater@sha256:[a-zA-Z0-9]{64}$/
     )
   })
 
   test('PROXY_IMAGE_NAME', () => {
     expect(PROXY_IMAGE_NAME).toMatch(
-      /^docker\.pkg\.github\.com\/github\/dependabot-update-job-proxy:v1$/
+      /^docker\.pkg\.github\.com\/github\/dependabot-update-job-proxy@sha256:[a-zA-Z0-9]{64}$/
     )
   })
 })
