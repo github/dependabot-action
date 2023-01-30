12ba47b7121097e81956b382156fbf271261558a
Jake Coffman <jake@jakecoffman.com>
2023-01-30 14:56:00 -0600

    pin ecosystem versions

--- src/fetch-images.ts
+++ src/fetch-images.ts
@@ -1,9 +1,15 @@
 import {ImageService} from './image-service'
-import {UPDATER_IMAGE_NAME, PROXY_IMAGE_NAME} from './docker-tags'
+import {updaterImageName, PROXY_IMAGE_NAME} from './docker-tags'
 
-export async function run(): Promise<void> {
-  await ImageService.pull(UPDATER_IMAGE_NAME)
+export async function run(packageManager: string): Promise<void> {
+  await ImageService.pull(updaterImageName(packageManager))
   await ImageService.pull(PROXY_IMAGE_NAME)
 }
 
-run()
+if (process.argv.length < 2) {
+  // eslint-disable-next-line no-console
+  console.error('Usage: npm run fetch-images <package-manager>')
+  process.exit(1)
+}
+
+run(process.argv[2])
