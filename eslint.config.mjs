987fa364e1fbeeca9637b8cd6aca61df46b96e67
Jeff Widman <jeff@jeffwidman.com>
2025-03-21 04:38:58 +0000

    Migrate to the new config file format
    Resolve the following error:
    ```
    (node:2525) ESLintIgnoreWarning: The ".eslintignore" file is no longer supported. Switch to using the "ignores" property in "eslint.config.js": https://eslint.org/docs/latest/use/configure/migration-guide#ignoring-files
    ```
    
    I migrated the files by following the steps in https://eslint.org/docs/latest/use/configure/migration-guide
    First step was using their programmatic migrator:
    ```
    npx  @eslint/migrate-config .eslintrc.json
    ```
    
    After that, I had to manually add the `ignore` object.

---
+++ eslint.config.mjs
@@ -0,0 +1,112 @@
+import { defineConfig } from "eslint/config";
+import github from "eslint-plugin-github";
+import jest from "eslint-plugin-jest";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("plugin:github/recommended", "plugin:github/typescript", "prettier"),
+
+    plugins: {
+        github,
+        jest,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...jest.environments.globals.globals,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 9,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.eslint.json",
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+    },
+
+    rules: {
+        "eslint-comments/no-use": "off",
+        "import/no-namespace": "off",
+        "no-unused-vars": "off",
+        camelcase: "off",
+        "@typescript-eslint/no-unused-vars": "error",
+
+        "@typescript-eslint/explicit-member-accessibility": ["error", {
+            accessibility: "no-public",
+        }],
+
+        "@typescript-eslint/no-require-imports": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/await-thenable": "error",
+        "@typescript-eslint/ban-ts-comment": "error",
+        "@typescript-eslint/consistent-type-assertions": "error",
+
+        "@typescript-eslint/explicit-function-return-type": ["error", {
+            allowExpressions: true,
+        }],
+
+        "@typescript-eslint/no-array-constructor": "error",
+        "@typescript-eslint/no-empty-interface": "error",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-extraneous-class": "error",
+        "@typescript-eslint/no-for-in-array": "error",
+        "@typescript-eslint/no-inferrable-types": "error",
+        "@typescript-eslint/no-misused-new": "error",
+        "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-non-null-assertion": "warn",
+        "@typescript-eslint/no-unnecessary-qualifier": "error",
+        "@typescript-eslint/no-unnecessary-type-assertion": "error",
+        "@typescript-eslint/no-useless-constructor": "error",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/prefer-for-of": "warn",
+        "@typescript-eslint/prefer-function-type": "warn",
+        "@typescript-eslint/prefer-includes": "error",
+        "@typescript-eslint/prefer-string-starts-ends-with": "error",
+        "@typescript-eslint/promise-function-async": "error",
+        "@typescript-eslint/require-array-sort-compare": "error",
+        "@typescript-eslint/restrict-plus-operands": "error",
+        "@typescript-eslint/unbound-method": "error",
+        "@typescript-eslint/no-shadow": "error",
+        "no-shadow": "off",
+        "i18n-text/no-en": "off",
+        "filenames/match-regex": "off",
+    },
+}, {
+    ignores: [
+        'dist/',
+        'lib/',
+        'node_modules/',
+        'jest.config.js',
+        'repo/',
+        'output/',
+    ],
+}, {
+    files: ["**/*.ts"],
+
+    rules: {
+        "no-undef": "off",
+    },
+}]);
