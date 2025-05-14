a5156e31e8e2adffedb794cd5cb371259ed2f6f9
Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>
2025-05-14 12:29:11 +0000

    Setting a certificate path

--- src/proxy.ts
+++ src/proxy.ts
@@ -223,7 +223,10 @@ export class ProxyBuilder {
         `JOB_ID=${jobId}`,
         `JOB_TOKEN=${jobToken}`,
         `PROXY_CACHE=${this.cachedMode ? 'true' : 'false'}`,
-        `DEPENDABOT_API_URL=${dependabotApiUrl}`
+        `DEPENDABOT_API_URL=${dependabotApiUrl}`,
+        `SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt`,
+        `REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt`,
+        `CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt`
       ],
       Entrypoint: [
         'sh',
