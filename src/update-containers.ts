e56c748caeaaa6a644435a45003d5f0aa3d070fd
Jamie Magee <jamie.magee@gmail.com>
2025-06-24 11:15:11 -0700

    Add `rust-toolchain` ecosystem support

--- src/update-containers.ts
+++ src/update-containers.ts
@@ -43,7 +43,8 @@ const manifest = {
   bun: getImageName('Dockerfile.bun'),
   docker_compose: getImageName('Dockerfile.docker-compose'),
   uv: getImageName('Dockerfile.uv'),
-  helm: getImageName('Dockerfile.helm')
+  helm: getImageName('Dockerfile.helm'),
+  rust_toolchain: getImageName('Dockerfile.rust-toolchain')
 }
 
 fs.writeFile(
