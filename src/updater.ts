f05211910b2ac43e8a562fe184e79a97bee7febb
Tom Christensen <pavera@github.com>
2025-08-14 18:06:05 +0000

    npm is special. treat it as such

--- src/updater.ts
+++ src/updater.ts
@@ -119,6 +119,9 @@ export class Updater {
     if (!credential.registry && credential.url) {
       try {
         obj.registry = new URL(credential.url).hostname
+        if (credential.type === 'npm_registry') {
+          obj.registry += `/${new URL(credential.url).pathname}`
+        }
       } catch {
         // If the URL is invalid, we skip setting the registry
         // as it will fall back to the default registry for the given type (e.g., npm, Docker, or Composer).
