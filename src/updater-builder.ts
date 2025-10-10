76ee713cfe4277c77a4238f78439d8d8613d64ef
Jake Coffman <jake@jakecoffman.com>
2025-10-10 14:56:48 -0500

    remove fetch_files command

--- src/updater-builder.ts
+++ src/updater-builder.ts
@@ -5,8 +5,6 @@ import {FileFetcherInput, FileUpdaterInput} from './config-types'
 import {JobParameters} from './inputs'
 import {Proxy} from './proxy'
 
-const JOB_OUTPUT_FILENAME = 'output.json'
-const JOB_OUTPUT_PATH = '/home/dependabot/dependabot-updater/output'
 const JOB_INPUT_FILENAME = 'job.json'
 const JOB_INPUT_PATH = `/home/dependabot/dependabot-updater`
 const REPO_CONTENTS_PATH = '/home/dependabot/dependabot-updater/repo'
@@ -38,7 +36,6 @@ export class UpdaterBuilder {
         `DEPENDABOT_JOB_TOKEN=`,
         `DEPENDABOT_JOB_PATH=${JOB_INPUT_PATH}/${JOB_INPUT_FILENAME}`,
         `DEPENDABOT_OPEN_TIMEOUT_IN_SECONDS=15`,
-        `DEPENDABOT_OUTPUT_PATH=${JOB_OUTPUT_PATH}/${JOB_OUTPUT_FILENAME}`,
         `DEPENDABOT_REPO_CONTENTS_PATH=${REPO_CONTENTS_PATH}`,
         `DEPENDABOT_API_URL=${this.jobParams.dependabotApiDockerUrl}`,
         `SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt`,
@@ -46,7 +43,6 @@ export class UpdaterBuilder {
         `HTTP_PROXY=${proxyUrl}`,
         `https_proxy=${proxyUrl}`,
         `HTTPS_PROXY=${proxyUrl}`,
-        `UPDATER_ONE_CONTAINER=1`,
         `ENABLE_CONNECTIVITY_CHECK=${
           process.env.DEPENDABOT_ENABLE_CONNECTIVITY_CHECK || '1'
         }`
