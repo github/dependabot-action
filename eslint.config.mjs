53966453634c121cfb7fcb5db8927e102fc87944
Jeff Widman <jeff@jeffwidman.com>
2026-08-10 12:36:30 -0700

    Use native flat Prettier config (#1750)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,24 +4,14 @@ import jest from "eslint-plugin-jest";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const githubConfigs = github.getFlatConfigs();
 
 export default defineConfig([
     js.configs.recommended,
-    ...compat.extends("prettier"),
+    eslintConfigPrettier,
     githubConfigs.recommended,
     githubConfigs.typescript,
     {
