b199507f988698f997872fcc141155f371c2a34c
Jamie Magee <jamagee@microsoft.com>
2026-09-01 11:44:55 -0700

    Rebuild Action distribution with Node 24

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -23695,6 +23695,12 @@ var require_utils3 = __commonJS({
   }
 });
 
+// node_modules/ssh2/lib/protocol/crypto/build/Release/sshcrypto.node
+var require_sshcrypto = __commonJS({
+  "node_modules/ssh2/lib/protocol/crypto/build/Release/sshcrypto.node"() {
+  }
+});
+
 // node_modules/ssh2/lib/protocol/crypto/poly1305.js
 var require_poly1305 = __commonJS({
   "node_modules/ssh2/lib/protocol/crypto/poly1305.js"(exports2, module2) {
@@ -24149,7 +24155,7 @@ var require_crypto = __commonJS({
     var ChaChaPolyDecipher;
     var GenericDecipher;
     try {
-      binding = require("./crypto/build/Release/sshcrypto.node");
+      binding = require_sshcrypto();
       ({
         AESGCMCipher,
         ChaChaPolyCipher,
